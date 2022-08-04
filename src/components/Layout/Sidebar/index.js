cf78ab4e754ca3358ffbb09307fecadfded679ce
Cody Polland <cody.polland@gmail.com>
2022-08-04 14:21:26 -0700

    nav bar set up start

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,14 +1,27 @@
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../../assests/images/logo-s.png'
 import logoSubtitle from '../../../assests/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesom/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
   <div className="nav-bar">
     <Link className="logo" to="/">
       <img src={LogoS} alt="logo" />
-      <img src={logoSubtitle} alt="" />
+      <img className="sub-logo" src={logoSubtitle} alt="" />
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e " />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e " />
+      </NavLink>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e " />
+      </NavLink>
+    </nav>
   </div>
 )
 
