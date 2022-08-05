b71e979bb02d2d973de04f83e5c924f0ef291607
Cody Polland <cody.polland@gmail.com>
2022-08-05 16:32:42 -0700

    scss for home page done

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,21 @@
 import LogoTitle from '../../assests/images/logo-s.png'
+import { Link } from 'react-router-dom'
+import './index.scss'
 
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          hi, <br /> I'm{' '}
+          hi, <br /> I'm <img src={LogoTitle} alt="developer" />
+          Cody
+          <br />
+          Web developer
         </h1>
-        <img src={LogoTitle} alt="developer" />
+        <h2>Frontend Developer / Designer</h2>
+        <Link to="/contact" className="flat-button">
+          Contact Me{' '}
+        </Link>
       </div>
     </div>
   )
