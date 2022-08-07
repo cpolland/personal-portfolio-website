970ca7dc814fe103f5b3a3901cce9f145fae6649
Cody Polland <cody.polland@gmail.com>
2022-08-07 13:45:44 -0700

    Animated latters added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,43 @@
 import LogoTitle from '../../assests/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import { useState } from 'react'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['l', 'o', 'b', 'o', 'd', 'a', 'n']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          hi, <br /> I'm <img src={LogoTitle} alt="developer" />
-          Cody
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass}_12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
           Web developer
         </h1>
