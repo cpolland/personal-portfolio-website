8ce1ce638e2aab8c5da9c934b6d04e0ed2652615
Cody Polland <cody.polland@gmail.com>
2022-08-17 13:32:14 -0700

    heroku update

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
-import { gsap } from 'gsap'
-import { DrawSVGPlugin } from 'gsap/DrawSVGPlugin'
+import gsap from 'gsap'
+import DrawSVGPlugin from 'gsap/DrawSVGPlugin.js'
 import LogoS from '../../../assets/images/logo-gray.png'
 import './index.scss'
 
