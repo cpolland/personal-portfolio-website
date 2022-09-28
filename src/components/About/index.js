4b8b59b18c5c2baa462119419d45587d0b9190e8
Cody Polland <cody.polland@gmail.com>
2022-09-27 17:27:52 -0700

    Media Queries

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -44,7 +44,7 @@ const About = () => {
             programs on the back-end. I love learning new langages and frame
             works when need be!
           </p>
-          <p align="LEFT">
+          <p>
             If someone were to describe me they would say I am a jack of all
             trades, a master of some. I thrive best in collaborative work
             environments and I am persevering . I like to work with teams but
@@ -66,26 +66,27 @@ const About = () => {
             CSS with Sass.
           </p>
         </div>
-
-        <div className="stage-cube-cont">
-          <div className="cubespinner">
-            <div className="face1">
-              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-            </div>
-            <div className="face2">
-              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-            </div>
-            <div className="face3">
-              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-            </div>
-            <div className="face4">
-              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-            </div>
-            <div className="face5">
-              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-            </div>
-            <div className="face6">
-              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="cube">
+          <div className="stage-cube-cont">
+            <div className="cubespinner">
+              <div className="face1">
+                <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+              </div>
+              <div className="face2">
+                <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+              </div>
+              <div className="face3">
+                <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+              </div>
+              <div className="face4">
+                <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+              </div>
+              <div className="face5">
+                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+              </div>
+              <div className="face6">
+                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+              </div>
             </div>
           </div>
         </div>
