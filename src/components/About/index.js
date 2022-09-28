e797c52cfa0aa610db930d5fcb30e4fe00e1a684
Cody Polland <cody.polland@gmail.com>
2022-09-28 13:13:32 -0700

    About Page Edit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -37,29 +37,29 @@ const About = () => {
             Colorado and currently living in San Diego, CA. I attended
             university at Savannah College of Art and Design and have my
             certification in full-stack web development from The University of
-            California San Diego UCSD. I learned full-stack web-dev but mainly
+            California San Diego UCSD. I studied full-stack web-dev but mainly
             focus on front-end development due to my love for design. I am
-            knowledgable of both visual design and UX design. I am efficient in
-            HTML, CSS, JavaScript, React, and I am familiar with a range of
-            programs on the back-end. I love learning new langages and frame
-            works when need be!
+            knowledgable of both UI/UX Design. I am efficient in HTML, CSS,
+            JavaScript, React, WordPress and I am familiar with a wide range of
+            programs on the back-end. I love learning new programming languages
+            and frame works when need be!
             <br></br>
             <br></br>
             If someone were to describe me they would say I am a jack of all
-            trades, a master of some. I thrive best in collaborative work
-            environments and I am persevering . I like to work with teams but
-            excel on my own as well. I have been in Web Development for a year
-            and strive to keep expanding my knowledge of all things coding.
+            trades. I thrive best in collaborative work environments and I am
+            perseverant when it comes to the success of my team. I have been in
+            Web Development for two years and strive to keep expanding my skill
+            set.
             <br></br>
             <br></br>
             What gives my life purpose is creating things I am passionate about.
-            Whether those things be tangible things or digital things; I enjoy
-            the creation process and the satisfaction of a finished product the
-            most. My hobbies include crocheting, illustration, photography,
-            sound design, sewing, and many more. I consider myself to be an
-            extremely compassionate person and I connect with pretty much anyone
-            who wants to connect with me. When I am not coding or crafting, you
-            can find me spending time with my boyfriend and my dog Riley.
+            Whether those things be tangible things or digital things. I enjoy
+            the creative process and the satisfaction of a finished product. My
+            hobbies include crocheting, illustration, photography, sound design,
+            sewing, and many more. I consider myself to be an extremely
+            compassionate person and I connect with pretty much anyone who wants
+            to connect with me. When I am not coding or crafting, you can find
+            me spending time with my boyfriend and my dog Riley.
             <br></br>
             <br></br>
             This app was created with: React - GSAP - FortAwesome - JavaScript -
